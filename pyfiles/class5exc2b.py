9052945870461ea94dfe5ac4f913f3c37ca50eec
BDUB21 <bdub0212121@gmail.com>
2019-06-06 13:18:48 -0700

    more class5 files

--- pyfiles/class5exc2b.py
+++ pyfiles/class5exc2b.py
@@ -1,16 +1,13 @@
 from __future__ import unicode_literals, print_function
 from jinja2 import FileSystemLoader, StrictUndefined
 from jinja2.environment import Environment
+import json
+import sys
 from pprint import pprint
 
 #env = Environment()
 env = Environment(undefined=StrictUndefined)
 env.loader = FileSystemLoader('.')
-
-#filename = 'nxos_config1.j2'
-#with open(filename) as f:
-#    nxos_template = f.read()
-
 
 nxos_vars = {
     "nxos_host1": 'nxos1',
@@ -22,9 +19,9 @@ nxos_vars = {
     "remote_as": 22,
     }
 
-
 nxos_template = 'nxos_config2.j2'
 exc3_template = env.get_template(nxos_template)
 #exc_template = Template(nxos_template)
 output = exc3_template.render(**nxos_vars)
+
 print(output)
